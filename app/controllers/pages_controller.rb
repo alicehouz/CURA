a8f6486cd631af872afc634d65d4fdec6fbc7992
Alice Houzelot <alicehouzelot@hotmail.fr>
2019-06-04 11:43:51 +0200

    homepage-front

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,7 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def home2
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_03_131133) do
+ActiveRecord::Schema.define(version: 2019_06_03_134009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,15 @@ ActiveRecord::Schema.define(version: 2019_06_03_131133) do
     t.datetime "updated_at", null: false
     t.index ["prescription_id"], name: "index_orders_on_prescription_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "orders_prescriptions", force: :cascade do |t|
+    t.bigint "order_id"
+    t.bigint "prescription_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_orders_prescriptions_on_order_id"
+    t.index ["prescription_id"], name: "index_orders_prescriptions_on_prescription_id"
   end
 
   create_table "prescriptions", force: :cascade do |t|
@@ -71,5 +80,7 @@ ActiveRecord::Schema.define(version: 2019_06_03_131133) do
 
   add_foreign_key "orders", "prescriptions"
   add_foreign_key "orders", "users"
+  add_foreign_key "orders_prescriptions", "orders"
+  add_foreign_key "orders_prescriptions", "prescriptions"
   add_foreign_key "prescriptions", "users", column: "users_id"
 end
