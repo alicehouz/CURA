a7e41d450162065fa7c276982bb1025e4c0a3bdd
Cybil LECHIGUERO <cybil.lechiguero@gmail.com>
2019-06-04 17:51:34 +0200

    user sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar, :street_name, :zip_code, :city])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar, :street_name, :zip_code, :city, :phone_number, :id_card, :social_security])
 
     # For additional in app/views/devise/registrations/edit.html.erb
     #devise_parameter_sanitizer.permit(:account_update, keys: [:username])
