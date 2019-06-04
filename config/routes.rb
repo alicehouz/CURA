d70558e6cbcf043815ff9b1119c7f51d56af67e2
Anthony Faoro <mamaaiiiing@gmail.com>
2019-06-04 16:43:54 +0200

    seed and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
 
-  resources :prescriptions, only: [:index, :new, :create, :show, :destroy]
-
+  resources :prescriptions, only: [:index, :new, :create, :show, :destroy] do
+    resources :orders, only: [:index, :new, :create, :show, :destroy]
+  end
   get 'welcome', to: "pages#welcome"
 
 
