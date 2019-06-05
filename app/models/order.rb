659976eeefdc9342ced83b700fda72a6b55d0908
Cybil LECHIGUERO <cybil.lechiguero@gmail.com>
2019-06-05 17:17:11 +0200

    controller view order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :user
-  has_many :prescriptions, through: :orders_prescription
+  has_many :orders_prescriptions, dependent: :destroy
+  has_many :prescriptions, through: :orders_prescriptions
+
   # belongs_to :pharmacy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-
   resources :prescriptions, only: [:index, :new, :create, :show, :destroy] do
     resources :orders, only: [:index, :new, :create]
   end
+  resources :orders, only: [:show, :destroy]
 
   get 'welcome', to: "pages#welcome"
 
