db2be79d35ceb17d82190ca2153553ecdc983155
marinaselim123 <marina.selim@kedgebs.com>
2019-06-07 14:14:53 +0200

    order process done

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,17 @@ Rails.application.routes.draw do
   resources :prescriptions, only: [:index, :new, :create, :show, :destroy] do
 
     get 'show_before_order', to: 'prescriptions#show_before_order'
-    resources :orders, only: [:new, :create, :show,:destroy]
 
-    resources :orders, only: [:index, :new, :create, :destroy]
+    get 'show_after_order', to: 'prescriptions#show_after_order'
+
+    resources :orders, only: [:index, :new, :create, :destroy, :show]
 
   end
-  resources :orders, only: [:index, :show, :destroy,:new, :create]
 
-
-
+  resources :orders, only: [:index, :show]
 
 
   get 'welcome', to: "pages#welcome"
-  get 'order_placement', to: "pages#order_placement"
-  get 'order_placement_2', to: "pages#order_placement_2"
-  get 'order_placement_3', to: "pages#order_placement_3"
 
   devise_for :users
 
