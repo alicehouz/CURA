adaf68852067ebc06e0e6781162cb7b0c0a36fd6
Cybil LECHIGUERO <cybil.lechiguero@gmail.com>
2019-06-07 11:25:17 +0200

    end scan and make an order

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -27,7 +27,25 @@ class PrescriptionsController < ApplicationController
     redirect_to prescriptions_path
   end
 
+  def create_simple
+    @prescription = Prescription.new(prescription_params_simple)
+    @prescription.user = current_user
+    if @prescription.save
+    redirect_to new_prescription_order_path(@prescription)
+    else
+      render :new_simple
+    end
+  end
+
+  def new_simple
+    @prescription = Prescription.new
+  end
+
   private
+
+  def prescription_params_simple
+    params.require(:prescription).permit(:image, :image_cache)
+  end
 
   def prescription_params
     params.require(:prescription).permit(:image, :image_cache, :prescription_name, :prescription_start_date, :prescription_expiry_date)
